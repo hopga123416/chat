d5cbf2eb4556e98688ab146feef8243ebb2188d5
hopga123416 <hopga123416@gmail.com>
2016-06-02 14:25:07 +0700

    s

--- app.js
+++ app.js
@@ -4,16 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var io = require('socket.io').listen(server);
-var port = process.env.PORT || 5000
 
-server.listen(port);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+app.io = require('socket.io')();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -59,39 +56,16 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-var usernames = {};
+app.io.on('connection', function(socket){
+console.log('a user connected');
 
-io.sockets.on('connection', function (socket) {
-
-  // when the client emits 'sendchat', this listens and executes
-  socket.on('sendchat', function (data) {
-    // we tell the client to execute 'updatechat' with 2 parameters
-    io.sockets.emit('updatechat', socket.username, data);
-  });
-
-  // when the client emits 'adduser', this listens and executes
-  socket.on('adduser', function(username){
-    // we store the username in the socket session for this client
-    socket.username = username;
-    // add the client's username to the global list
-    usernames[username] = username;
-    // echo to client they've connected
-    socket.emit('updatechat', 'SERVER', 'you have connected');
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
-    // update the list of users in chat, client-side
-    io.sockets.emit('updateusers', usernames);
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', function(){
-    // remove the username from global usernames list
-    delete usernames[socket.username];
-    // update list of users in chat, client-sidez
-    io.sockets.emit('updateusers', usernames);
-    // echo globally that this client has left
-    socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-  });
+// receive from client (index.ejs) with socket.on
+socket.on('new message', function(msg){
+      console.log('new message: ' + msg);
+      // send to client (index.ejs) with app.io.emit
+      // here it reacts direct after receiving a message from the client
+      app.io.emit('chat message' , msg);
+      });
 });
 
 module.exports = app;
